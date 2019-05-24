4eebe2bb22d000aa22a83e8abea3e1e3462dc728
sirm2z <sirm2zisv@gmail.com>
2019-05-24 22:05:02 +0800

    update

--- src/generate-html.js
+++ src/generate-html.js
@@ -1,20 +1,53 @@
 /**
- * convert md to inline html
+ * convert md to html
  */
 const fs = require('fs');
 const path = require('path');
-const marked = require('marked');
-const { htmlTemplate, markdownStr } = require('./template');
+const md = require('markdown-it');
+const mdPluginSpan = require('./markdown-it-span');
 
-const markdownElm = marked(markdownStr);
-const html = htmlTemplate.replace(/%markdown-content%/g, markdownElm);
+const appDirectory = fs.realpathSync(process.cwd());
 
-fs.open(path.resolve(__dirname, 'index.html'), 'w', (e, fd) => {
-  if (e) throw e;
-  fs.write(fd, html, 'utf8', (e) => {
-    if (e) throw e;
-    fs.closeSync(fd);
-    console.log('index.html 生成完毕');
+function readFilePromise(filePath) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) {
+        reject(err);
+      }
+      resolve(data);
+    });
+  })
+}
+
+// 获取 markdown string
+function getMarkdownStr() {
+  return readFilePromise(path.resolve(__dirname, 'doc.md')).then(res => {
+    return new md().use(mdPluginSpan).render(res);
   });
-});
+}
 
+// 生成 html string
+function generateHtmlStr(markdownElm, title) {
+  return readFilePromise(path.resolve(__dirname, 'template.html')).then(res => {
+    return res.replace(/%markdown-title%/g, title).replace(/%markdown-content%/g, markdownElm);
+  });
+}
+
+// 写入 index.html
+function writeIndexHtml(html) {
+  fs.open(path.resolve(appDirectory, 'index.html'), 'w', (e, fd) => {
+    if (e) throw e;
+    fs.write(fd, html, 'utf8', (e) => {
+      if (e) throw e;
+      fs.closeSync(fd);
+    });
+  });
+}
+
+(async function() {
+  const title = "毛润泽的简历";
+  const markdownElm = await getMarkdownStr();
+  const html = await generateHtmlStr(markdownElm, title);
+  await writeIndexHtml(html);
+  console.log('index.html 生成完毕');
+})();
---
+++ src/markdown-it-span.js
@@ -0,0 +1,67 @@
+// [[span]]
+//
+
+const MARKER_OPEN = "[";
+const MARKER_CLOSE = "]";
+const TAG = "span";
+
+/*
+ * Add delimiters for double occurrences of MARKER_SYMBOL.
+ */
+function tokenize(state, silent) {
+  if (silent) {
+    return false;
+  }
+
+  const start = state.pos;
+  const max = state.posMax;
+  let momChar = state.src.charAt(start);
+  let nextChar = state.src.charAt(start + 1);
+
+  // we're looking for two times the open symbol.
+  if (momChar !== MARKER_OPEN || nextChar !== MARKER_OPEN) {
+    return false;
+  }
+
+  // find the end sequence
+  let end = -1;
+  nextChar = state.src.charAt(start + 2);
+  for (let i = start + 2; i < max && end === -1; i++) {
+    momChar = nextChar;
+    nextChar = state.src.charAt(i + 1);
+    if (momChar === MARKER_CLOSE && nextChar === MARKER_CLOSE) {
+      // found the end!
+      end = i;
+    }
+    if (momChar === MARKER_OPEN && momChar === MARKER_OPEN) {
+      // found another opening sequence before the end. Thus, ignore ours!
+      return false;
+    }
+    if (momChar === "\n") {
+      // found end of line before the end sequence. Thus, ignore our start sequence!
+      return false;
+    }
+  }
+
+  // input ended before closing sequence
+  if (end === -1) {
+    return false;
+  }
+
+  // start tag
+  state.push("span_open", TAG, 1);
+  // parse inner
+  state.pos += 2;
+  state.posMax = end;
+  state.md.inline.tokenize(state);
+  state.pos = end + 2;
+  state.posMax = max;
+  // end tag
+  state.push("span_close", TAG, -1);
+
+  return true;
+}
+
+module.exports = function spanplugin(markdownit) {
+  markdownit.inline.ruler.before("link", "span", tokenize);
+};
